db8a391515b09ad0100537e51181bd9263e2b02d
Odilbek <=>
2025-12-08 14:34:04 +0500

    c

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -3,20 +3,21 @@ from .models import Gifts, Order, QRCode
 
 @admin.register(Gifts)
 class GiftsAdmin(admin.ModelAdmin):
-    list_display = ('name', 'available', 'order_number')
-    list_filter = ('available',)
+    list_display = ('name', 'price', 'count')
+    list_filter = ('price',)
     search_fields = ('name',)
-    list_editable = ('available',)
+    list_editable = ('count',)
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'gift', 'customer_name', 'customer_phone', 'order_date')
-    list_filter = ('order_date',)
-    search_fields = ('customer_name', 'customer_phone', 'gift__name')
+    list_display = ('id', 'gift', 'order_date', 'is_viewed')
+    list_filter = ('order_date', 'is_viewed')
+    search_fields = ('gift__name',)
     readonly_fields = ('order_date',)
 
 @admin.register(QRCode)
 class QRCodeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'qr_image', 'available')
-    list_filter = ('available',)
+    list_display = ('id', 'qr_image', 'available', 'created_at')
+    list_filter = ('available', 'created_at')
     list_editable = ('available',)
+
